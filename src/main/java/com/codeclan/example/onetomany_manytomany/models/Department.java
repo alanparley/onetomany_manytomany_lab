465dc30db2c16935935240e4bf69fa165a112d41
Alan Parley <alanparley@hotmail.com>
2020-12-01 10:50:47 +0000

    Department class setup

--- src/main/java/com/codeclan/example/onetomany_manytomany/models/Department.java
+++ src/main/java/com/codeclan/example/onetomany_manytomany/models/Department.java
@@ -1,23 +1,39 @@
 package com.codeclan.example.onetomany_manytomany.models;
 
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name="departments")
 public class Department {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column (name = "id")
     private Long id;
 
     @Column (name = "name")
     private String name;
 
-    public Department(){}
+    @JsonIgnoreProperties({"department"})
+    @OneToMany(mappedBy = "department")
+    private List<Employee> employees;
 
     public Department(String name) {
         this.name = name;
+        this.employees = new ArrayList<>();
+    }
+
+    public Department(){}
+
+    public List<Employee> getEmployees() {
+        return employees;
+    }
+
+    public void setEmployees(List<Employee> employees) {
+        this.employees = employees;
     }
 
     public Long getId() {
--- src/test/java/com/codeclan/example/onetomany_manytomany/OnetomanyManytomanyApplicationTests.java
+++ src/test/java/com/codeclan/example/onetomany_manytomany/OnetomanyManytomanyApplicationTests.java
@@ -1,13 +1,39 @@
 package com.codeclan.example.onetomany_manytomany;
 
+import com.codeclan.example.onetomany_manytomany.models.Department;
+import com.codeclan.example.onetomany_manytomany.models.Employee;
+import com.codeclan.example.onetomany_manytomany.repositories.DepartmentRepository;
+import com.codeclan.example.onetomany_manytomany.repositories.EmployeeRepository;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class OnetomanyManytomanyApplicationTests {
 
+	@Autowired
+	EmployeeRepository employeeRepository;
+
+	@Autowired
+	DepartmentRepository departmentRepository;
+
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	public void createEmployeeAndDepartment(){
+		Department department = new Department("Accounts");
+		departmentRepository.save(department);
+
+		Employee employee1 = new Employee("Amy", "Browne", 1389);
+		employeeRepository.save(employee1);
+		Employee employee2 = new Employee("Alan", "Parley", 7892);
+		employeeRepository.save(employee2);
+
 	}
 
 }
